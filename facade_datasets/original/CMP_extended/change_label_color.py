a0e58f114d73075d58ee3d9c20b504ced7fa5b81
pineapplebun <kevinjr.chen1@gmail.com>
2017-07-14 20:36:40 -0400

    made a modified dataset of only window and non-window classes

---
+++ facade_datasets/original/CMP_extended/change_label_color.py
@@ -0,0 +1,39 @@
+from PIL import Image
+import os
+from tkinter import filedialog
+
+# Hyperparameters
+new_color = (0,0,0) # Define the non-feature color
+
+# Find folder directory 
+directory = filedialog.askdirectory()
+images = os.listdir(directory)
+for file in images[:]:
+    if not(file.endswith(".png")):
+           images.remove(file)
+
+# Make output directory
+new_dir = directory + "/out"
+if not os.path.exists(new_dir):
+    os.makedirs(new_dir)
+
+for image in images:
+
+    picture = Image.open(directory + "/" + image)
+    picture = picture.convert('RGB')
+
+    # Get the size of the image
+    width, height = picture.size
+
+    image_name = image.split(".")[0]
+    
+    # Process every pixel
+    for x in range(width):
+        for y in range(height):
+            red, green, blue = picture.getpixel((x,y))
+            if (red == 255 and green == 255 and blue == 0) or (red == 0 and green == 85 and blue == 255):
+                picture.putpixel( (x,y), (255,0,0))
+            else:
+               picture.putpixel( (x,y), new_color)
+
+    picture.save(new_dir + "/" + image_name + ".png", "png")
