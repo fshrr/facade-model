813a78e3a6cd8085b9902eead5fa2da208725eac
pineapplebun <kevinjr.chen1@gmail.com>
2017-07-28 01:02:15 -0400

    refactored detect_edge

--- edge/detect_edge.py
+++ edge/detect_edge.py
@@ -8,61 +8,66 @@ from skimage.filters import roberts, sobel, scharr, prewitt
 import scipy.misc
 import os
 
-cwd = os.getcwd()
-input_dir = cwd + "/in"
-if not os.path.exists(input_dir):
-    input_dir = os.filedialog.askfilenamedirectory()
+def return_input_folder_location():
+    """
+    Return the input folder location. If "input_images" does not exist in the
+    parent folder of the edge folder, then ask the user for an input folder.
+    Else, return the input folder absolute path of the input_images folder.
+    """
+    os.chdir("..")
+    input_dir = os.getcwd() + "/input_images"
+    if not os.path.exists(input_dir):
+        input_dir = os.filedialog.askfilenamedirectory()
+        return input_dir
+    else:
+        os.chdir("input_images")
+        input_dir = os.getcwd()
+        return input_dir
 
-mid_dir = cwd + "/mid"
-if not os.path.exists(mid_dir):
-    os.makedirs(mid_dir)
-    
-images = os.listdir(input_dir)
+def create_intermediate_folder():
+    cwd = os.getcwd()
+    mid_dir = cwd + "/mid"
+    if not os.path.exists(mid_dir):
+        os.makedirs(mid_dir)
 
-for image in images:
-    image_name = image.split(".")[0]
-    im = rgb2gray(io.imread(input_dir + "/" + image))
+def detect_edges(input_dir, method=6, sigma=2):
+    """
+    Detect edges using an edge detection method.
+    The default method is prewitt.
 
-    #edges1 = feature.canny(im)
-    #edges2 = feature.canny(im, sigma=2)
-    #edges3 = roberts(im)
-    #edges4 = sobel(im)
-    #edges5 = scharr(im)
-    edges6 = prewitt(im)
+    1. Default skimage Canny
+    2. Canny w/ sigma=2
+    3. Roberts
+    4. Sobel
+    5. Scharr
+    6. Prewitt
 
-    scipy.misc.imsave(mid_dir + "/" + image_name + '.png', edges6)
- 
-"""
-fig, (ax1, ax2, ax3, ax4, ax5, ax6) = plt.subplots(nrows=1, ncols=6, figsize=(10,5),
-                                    sharex=True, sharey=True)
+    Note that Prewitt is default because it currently works best with facades.
 
-ax1.imshow(edges1, cmap=plt.cm.gray)
-ax1.axis('off')
-ax1.set_title('default sigma', fontsize=20)
+    @params input_dir : String
+    @params method : int
+    @params sigma : float or int
+    """
+    images = os.listdir(input_dir)
 
-ax2.imshow(edges2, cmap=plt.cm.gray)
-ax2.axis('off')
-ax2.set_title('$\sigma=3$', fontsize=20)
+    def edge_method(method_num, im, sigma=2):
+        return {
+            1:feature.canny(im),
+            2:feature.canny(im, sigma=2),
+            3:roberts(im),
+            4:sobel(im),
+            5:scharr(im),
+            6:prewitt(im)
+        }
 
-ax3.imshow(edges3, cmap=plt.cm.gray)
-ax3.axis('off')
-ax3.set_title('Roberts', fontsize=20)
+    for image in images:
+        image_name = image.split(".")[0]
+        im = rgb2gray(io.imread(input_dir + "/" + image))
 
-ax4.imshow(edges3, cmap=plt.cm.gray)
-ax4.axis('off')
-ax4.set_title('Sobel', fontsize=20)
+        if method is int and 1 <= method <=6:
+            scipy.misc.imsave(mid_dir + "/" + image_name + '.png', edge_method(6,im,sigma))
 
-ax5.imshow(edges3, cmap=plt.cm.gray)
-ax5.axis('off')
-ax5.set_title('Scharr', fontsize=20)
-
-ax6.imshow(edges3, cmap=plt.cm.gray)
-ax6.axis('off')
-ax6.set_title('Prewitt', fontsize=20)
-
-fig.tight_layout()
-"""
-
-#plt.show()
-
-
+if __name__ == "__main__":
+    create_intermediate_folder()
+    input_folder_location = return_input_folder_location()
+    detect_edges(input_folder_location)
